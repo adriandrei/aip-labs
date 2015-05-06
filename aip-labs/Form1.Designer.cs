b946717f3a66507ea02f67a461b2a6e0d86eb300
adriandrei <andrei.ionutadrian@gmail.com>
2015-05-07 00:57:39 +0300

    Added Lab2

---
+++ aip-labs/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace aip_labs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.OutputArea = new System.Windows.Forms.RichTextBox();
+            this.Lab2Generare = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.Lab2Secvential = new System.Windows.Forms.Button();
+            this.Lab2Paralel = new System.Windows.Forms.Button();
+            this.Lab2Numeric = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Lab2Numeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Controls.Add(this.tabPage7);
+            this.tabControl1.Location = new System.Drawing.Point(254, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(277, 274);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.Lab2Numeric);
+            this.tabPage1.Controls.Add(this.Lab2Paralel);
+            this.tabPage1.Controls.Add(this.Lab2Secvential);
+            this.tabPage1.Controls.Add(this.richTextBox2);
+            this.tabPage1.Controls.Add(this.Lab2Generare);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(269, 248);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Lab 2";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(155, 252);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lab 3";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // OutputArea
+            // 
+            this.OutputArea.Location = new System.Drawing.Point(13, 13);
+            this.OutputArea.Name = "OutputArea";
+            this.OutputArea.Size = new System.Drawing.Size(235, 273);
+            this.OutputArea.TabIndex = 1;
+            this.OutputArea.Text = "";
+            // 
+            // Lab2Generare
+            // 
+            this.Lab2Generare.Location = new System.Drawing.Point(7, 120);
+            this.Lab2Generare.Name = "Lab2Generare";
+            this.Lab2Generare.Size = new System.Drawing.Size(75, 23);
+            this.Lab2Generare.TabIndex = 0;
+            this.Lab2Generare.Text = "Generare";
+            this.Lab2Generare.UseVisualStyleBackColor = true;
+            this.Lab2Generare.Click += new System.EventHandler(this.Lab2Generare_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(155, 252);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Lab 4";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(155, 252);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Lab 5";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(155, 252);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Lab 6";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(155, 252);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Lab 7";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(155, 252);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Lab 8";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Enabled = false;
+            this.richTextBox2.Location = new System.Drawing.Point(7, 7);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(256, 107);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = resources.GetString("richTextBox2.Text");
+            // 
+            // Lab2Secvential
+            // 
+            this.Lab2Secvential.Location = new System.Drawing.Point(7, 149);
+            this.Lab2Secvential.Name = "Lab2Secvential";
+            this.Lab2Secvential.Size = new System.Drawing.Size(75, 23);
+            this.Lab2Secvential.TabIndex = 2;
+            this.Lab2Secvential.Text = "Secvential";
+            this.Lab2Secvential.UseVisualStyleBackColor = true;
+            this.Lab2Secvential.Click += new System.EventHandler(this.Lab2Secvential_Click);
+            // 
+            // Lab2Paralel
+            // 
+            this.Lab2Paralel.Location = new System.Drawing.Point(7, 179);
+            this.Lab2Paralel.Name = "Lab2Paralel";
+            this.Lab2Paralel.Size = new System.Drawing.Size(75, 23);
+            this.Lab2Paralel.TabIndex = 3;
+            this.Lab2Paralel.Text = "Paralel";
+            this.Lab2Paralel.UseVisualStyleBackColor = true;
+            this.Lab2Paralel.Click += new System.EventHandler(this.Lab2Paralel_Click);
+            // 
+            // Lab2Numeric
+            // 
+            this.Lab2Numeric.Location = new System.Drawing.Point(143, 123);
+            this.Lab2Numeric.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.Lab2Numeric.Name = "Lab2Numeric";
+            this.Lab2Numeric.Size = new System.Drawing.Size(120, 20);
+            this.Lab2Numeric.TabIndex = 4;
+            this.Lab2Numeric.Value = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nr  Ec:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 300);
+            this.Controls.Add(this.OutputArea);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "AIP";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Lab2Numeric)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox OutputArea;
+        private System.Windows.Forms.Button Lab2Generare;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button Lab2Paralel;
+        private System.Windows.Forms.Button Lab2Secvential;
+        private System.Windows.Forms.NumericUpDown Lab2Numeric;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ aip-labs/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aip_labs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Stopwatch sw;
+        private double[] a;
+        private double[] b;
+        private double[] c;
+
+        private void Lab2Generare_Click(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Start();
+
+            a = new double[(int)Lab2Numeric.Value];
+            b = new double[(int)Lab2Numeric.Value];
+            c = new double[(int)Lab2Numeric.Value];
+
+            var rand = new Random();
+            for(int i = 0; i < (int)Lab2Numeric.Value; i++)
+            {
+                a[i] = rand.Next(1000);
+                b[i] = rand.Next(1000);
+                c[i] = rand.Next(1000);
+            }
+            sw.Stop();
+            OutputArea.Text = string.Format("Lab 2 : \nS-au generat {0} coeficienti in {1}ms..", Lab2Numeric.Value, sw.ElapsedMilliseconds);
+        }
+
+        private void Lab2Secvential_Click(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Start();
+
+            for(int i = 0; i < (int)Lab2Numeric.Value; i++)
+            {
+                var delta = b[i] * b[i] - 4 * a[i] * c[i];
+                if (delta < 0)
+                    continue;
+                if (delta == 0)
+                {
+                    double result = ((-1) * b[i]) / (2 * a[i]);
+                    continue;
+                }
+                double result1 = ((-1) * b[i] + delta) / (2 * a[i]);
+                double result2 = ((-1) * b[i] - delta) / (2 * a[i]);
+            }
+
+            sw.Stop();
+            OutputArea.Text += string.Format("\nSecvential in {0}ms...", sw.ElapsedMilliseconds);
+        }
+
+        private void Lab2Paralel_Click(object sender, EventArgs e)
+        {
+            sw = new Stopwatch();
+            sw.Start();
+
+            Parallel.For(0, (int)Lab2Numeric.Value, i => {
+                var delta = b[i] * b[i] - 4 * a[i] * c[i];
+                if (delta < 0)
+                    return;
+                if (delta == 0)
+                {
+                    double result = ((-1) * b[i]) / (2 * a[i]);
+                    return;
+                }
+                double result1 = ((-1) * b[i] + delta) / (2 * a[i]);
+                double result2 = ((-1) * b[i] - delta) / (2 * a[i]);
+            });
+
+            sw.Stop();
+            OutputArea.Text += string.Format("\nParalel in {0}ms...", sw.ElapsedMilliseconds);
+        }
+    }
+}
